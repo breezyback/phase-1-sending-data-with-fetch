51b1ac0e4f209fcfc8ac716ba5016c1402cf1a80
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-30 15:58:58 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({ name, email })
+    }
+
+    fetch("http://localhost:3000/users", configurationObject)
+        .then(res => res.json())
+        .then(data => data)
+        .catch(err => err.message);
+}
